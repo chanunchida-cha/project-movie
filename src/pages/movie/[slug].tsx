761560529baaeeaa2b297c5e89ab4f01e4ff2bb5
Yok <yok.c@jenosize.com>
2023-01-25 11:52:49 +0700

    [update] create movie detail route

---
+++ src/pages/movie/[slug].tsx
@@ -0,0 +1,10 @@
+import { useRouter } from "next/router";
+import React, { ReactElement } from "react";
+
+interface Props {}
+
+function MovieDetail({}: Props): ReactElement {
+  return <div></div>;
+}
+
+export default MovieDetail;
