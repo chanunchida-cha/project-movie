541d2b32a51498138768a66ee117cf6420472780
Yok <yok.c@jenosize.com>
2023-01-25 14:44:00 +0700

    [update]  edite condition hidden banner

--- src/layouts/Header.tsx
+++ src/layouts/Header.tsx
@@ -1,14 +1,19 @@
 import Banner from "@/ui/Banner";
 import Navbar from "@/ui/Navbar";
+import { useRouter } from "next/router";
 import React, { ReactNode } from "react";
 
 type Props = {};
 
 const Header = (props: Props) => {
+  const route = useRouter();
+  console.log(route);
+  const hiddenBanner = route.pathname.startsWith("/movie");
+
   return (
     <>
       <Navbar />
-      <Banner />
+      {!hiddenBanner && <Banner />}
     </>
   );
 };
