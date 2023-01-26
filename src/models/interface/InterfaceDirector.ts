cd9371a080063abebe23cca4f0d7c5f84d255cf9
Yok <yok.c@jenosize.com>
2023-01-26 12:46:13 +0700

    edit name interface

--- src/models/interface/InterfaceDirector.ts
+++ src/models/interface/InterfaceDirector.ts
@@ -1,17 +1,19 @@
 export interface Director {
   name: string;
   img: string;
-  des: Des;
+  des: DesDirector;
   knownFor: KnownFor[];
 }
 
-export interface Des {
+export interface DesDirector {
+  [key:string]:string
   alternativeNames: string;
   height: string;
   born: string;
   parents: string;
   relatives: string;
   directed: string;
+
   
 }
 
